a456f4eee9cf1877031c2e3f6141612a1504268f
hyun-hyun <59357000+hyun-hyun@users.noreply.github.com>
2020-01-06 18:41:04 +0900

    inventory
    inventory

---
+++ Assets/Scripts/EscapeButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeButton : MonoBehaviour
+{
+    public GameObject SlotPanel;
+
+    public void EscapeButtonClicked()
+    {
+        SlotPanel.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/ItemController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+using UnityEngine.UI;
+
+[Serializable]
+public class Item
+{
+    public int IID;
+    public string name;
+    public string description;
+    public string image;
+}
+
+[Serializable]
+public class ItemDB
+{
+    public List<Item> ItemList;
+}
+
+public class ItemController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static ItemDB IL = new ItemDB();
+    public GameObject SlotHolder;
+
+    TextAsset textData;
+
+    void Start()
+    {
+        textData=Resources.Load("DB/ItemList") as TextAsset;
+        IL = JsonUtility.FromJson<ItemDB>(textData.ToString());
+        Debug.Log(IL.ItemList[0].name);
+        Debug.Log("sss");
+    }
+
+}
