6d43fc0743fb970f6d8a274ac40704c849dafd87
ethrad <55068101+ethrad@users.noreply.github.com>
2020-01-07 16:23:49 +0900

    story update

--- Assets/Scripts/StoryController.cs
+++ Assets/Scripts/StoryController.cs
@@ -6,7 +6,7 @@ using System;
 using UnityEngine.UI;
 
 [Serializable]
-public class Story
+public class Script
 {
     public string background;
     public string bgm;
@@ -14,28 +14,49 @@ public class Story
 }
 
 [Serializable]
-public class StoryList {
-    public List<Story> SL;
+public class StoryList
+{
+    public List<Script> Scripts;
+}
+
+[Serializable]
+public class StoryDB
+{
+    public List<StoryList> SL;
 }
 
 public class StoryController : MonoBehaviour
 {
-    public static StoryList storyList = new StoryList();
+    public static StoryDB storyList = new StoryDB();
     public GameObject background;
     public GameObject script;
     TextAsset textData;
+    private int SN;
+    public static int storyNum = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         textData = Resources.Load("DB/StoryList") as TextAsset;
-        storyList = JsonUtility.FromJson<StoryList>(textData.ToString());
-        Debug.Log(storyList.SL[0].background);
+        storyList = JsonUtility.FromJson<StoryDB>(textData.ToString());
+        SN = 0;
+        SceneSetting();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (SN == storyList.SL[storyNum].Scripts.Count-1) { }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            SN++;
+            SceneSetting();
+        }
+    }
+
+    void SceneSetting()
+    {
+        background.transform.GetComponent<Image>().sprite = Resources.Load<Sprite>("Image/" + storyList.SL[storyNum].Scripts[SN].background);
+        script.transform.GetComponent<Text>().text = storyList.SL[storyNum].Scripts[SN].script;
     }
 }
