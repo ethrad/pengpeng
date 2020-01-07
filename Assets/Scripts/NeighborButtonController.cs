00f3dc36b7eac1efe996dcd2ce59d2151546879e
hyun-hyun <59357000+hyun-hyun@users.noreply.github.com>
2020-01-07 15:40:06 +0900

    edit panel

---
+++ Assets/Scripts/NeighborButtonController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeighborButtonController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject NeighborPanel;
+
+    public void OnNeighborButtonClicked()
+    {
+        NeighborPanel.SetActive(true);
+        neighborPanelUpdate();
+    }
+
+    // Update is called once per frame
+    void neighborPanelUpdate()
+    {
+        
+    }
+}
