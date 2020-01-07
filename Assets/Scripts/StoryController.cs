70f7067be32117da1912dcea339a31e55f9945a1
ethrad <55068101+ethrad@users.noreply.github.com>
2020-01-07 16:03:11 +0900

    story update

--- Assets/Scripts/StoryController.cs
+++ Assets/Scripts/StoryController.cs
@@ -1,13 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System;
+using UnityEngine.UI;
+
+[Serializable]
+public class Story
+{
+    public string background;
+    public string bgm;
+    public string script;
+}
+
+[Serializable]
+public class StoryList {
+    public List<Story> SL;
+}
 
 public class StoryController : MonoBehaviour
 {
+    public static StoryList storyList = new StoryList();
+    public GameObject background;
+    public GameObject script;
+    TextAsset textData;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        textData = Resources.Load("DB/StoryList") as TextAsset;
+        storyList = JsonUtility.FromJson<StoryList>(textData.ToString());
+        Debug.Log(storyList.SL[0].background);
     }
 
     // Update is called once per frame
