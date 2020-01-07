6e56dc3d9d9b1c19012461745fe047e8a36ea3fd
hyun-hyun <59357000+hyun-hyun@users.noreply.github.com>
2020-01-07 14:34:44 +0900

    Revert "story update"
    This reverts commit e4bf7b2b135b5b992c45597c9fd5b3f91c9ea480.

--- Assets/Scripts/StoryController.cs
+++ Assets/Scripts/StoryController.cs
@@ -1,43 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using System;
-using System.IO;
-
-[Serializable]
-public class Story {
-    public string background;
-    public string script;
-}
 
 public class StoryController : MonoBehaviour
 {
-    private int SN;
-    public static List<Story> StoryList;
-    public GameObject background;
-    public GameObject script;
-
-    TextAsset textData;
     // Start is called before the first frame update
     void Start()
     {
-        SN = 0; //scene number;
-        textData = Resources.Load("DB/StoryList") as TextAsset;
-        StoryList = JsonUtility.FromJson<List<Story>>(textData.ToString());
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            SN++;
-            SceneSetting();
-        }
-    }
-
-    void SceneSetting() {
-
+        
     }
 }
