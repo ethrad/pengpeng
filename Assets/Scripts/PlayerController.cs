820fcefe860239bdb7b55b3ca0dcb72a7576680b
ethrad <55068101+ethrad@users.noreply.github.com>
2020-01-16 18:15:24 +0900

    정보 가져오기 테스트는 안함

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,17 @@ public class Player
     public int talkNum;
     public int dayNum;
     public int deadNum;
+
+    public Player()
+    {
+    }
+    public Player(int money, int talkNum, int dayNum, int deadNum)
+    {
+        this.money = money;
+        this.talkNum = talkNum;
+        this.dayNum = dayNum;
+        this.deadNum = deadNum;
+    }
 }
 
 public class PlayerController : MonoBehaviour
@@ -30,9 +41,9 @@ public class PlayerController : MonoBehaviour
         playerInfo.transform.GetComponent<Text>().text = "돈 : " + player.money + "   D-" + player.dayNum + "\n남은 대화 횟수" + player.talkNum;
     }
 
-    // Update is called once per frame
-    void Update()
+    void playerInfoUpdate(int money, int talkNum, int dayNum, int deadNum)
     {
-
+        string jsonStr = JsonUtility.ToJson(new Player(money, talkNum, dayNum, deadNum));
+        File.WriteAllText(Application.dataPath + "/Resources/Player.json", jsonStr);
     }
 }
