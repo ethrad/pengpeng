add47b5c02e0d511d02f42b8d682d0faf612fdf1
ethrad <55068101+ethrad@users.noreply.github.com>
2020-01-16 18:06:09 +0900

    주인공 정보 띄우기

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+using UnityEngine.UI;
+
+
+[System.Serializable]
+public class Player
+{
+    public int money;
+    public int talkNum;
+    public int dayNum;
+    public int deadNum;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject playerInfo;
+    TextAsset textData;
+    Player player = new Player();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textData = Resources.Load("DB/Player") as TextAsset;
+        player = JsonUtility.FromJson<Player>(textData.ToString());
+        playerInfo.transform.GetComponent<Text>().text = "돈 : " + player.money + "   D-" + player.dayNum + "\n남은 대화 횟수" + player.talkNum;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
