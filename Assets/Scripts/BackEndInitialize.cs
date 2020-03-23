b55121230557396a2482f3342f7445d117e3447c
ethrad <55068101+ethrad@users.noreply.github.com>
2020-03-23 22:54:26 +0900

    아니 왜 빌드 안돼

---
+++ Assets/Scripts/BackEndInitialize.cs
@@ -0,0 +1,35 @@
+﻿using BackEnd;
+using UnityEngine;
+
+public class BackEndInitialize : MonoBehaviour
+{
+
+    // .Net 4 버전
+    void Awake()
+    {
+        Backend.Initialize(HandleBackendCallback);
+    }
+
+    void HandleBackendCallback()
+    {
+        if (Backend.IsInitialized)
+        {
+            Debug.Log("뒤끝SDK 초기화 완료");
+
+            // example 
+            // 버전체크 -> 업데이트 
+
+            // 구글 해시키 획득 
+            if (!Backend.Utils.GetGoogleHash().Equals(""))
+                Debug.Log(Backend.Utils.GetGoogleHash());
+
+            // 서버시간 획득
+            Debug.Log(Backend.Utils.GetServerTime());
+        }
+        // 실패
+        else
+        {
+            Debug.LogError("Failed to initialize the backend");
+        }
+    }
+}
